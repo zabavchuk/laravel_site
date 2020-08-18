98a04a223fa986f704e50e01836a46ae6bebe54f
Daniel Zabavchuk <d.p.zabavchuk@gmail.com>
2020-08-18 22:35:31 +0300

    +

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>My Laravel project</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    My Laravel project
                 </div>
 
                 <div class="links">
