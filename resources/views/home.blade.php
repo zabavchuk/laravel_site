74ae1f3dfbbb7849a329c3bc5ebf08f7d449114a
Daniel Zabavchuk <d.p.zabavchuk@gmail.com>
2020-08-19 04:38:23 +0300

    +

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fiev1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/34565799_438171569943658_196224265341632512_n.jpg?_nc_ht=instagram.fiev1-1.fna.fbcdn.net&_nc_ohc=p6pOrtMcKlUAX9euqFy&oh=c3b4121d1fd7f30e1840b2bcd99e7718&oe=5F67768F" alt="" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>D.P.Creative</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>998m</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
         </div>
     </div>
