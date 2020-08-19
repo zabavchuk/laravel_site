c84415bee84f30bac741c651a445503087216fa3
Daniel Zabavchuk <d.p.zabavchuk@gmail.com>
2020-08-20 02:39:41 +0300

    day3

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\Profile;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-    public function index()
+    public function index($user_id)
     {
-        return view('home');
+
+        $template = [
+            'user' => User::findOrFail($user_id)
+        ];
+
+        return view('profiles/index', $template);
     }
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,15 +7,18 @@
             <img src="https://instagram.fiev1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/34565799_438171569943658_196224265341632512_n.jpg?_nc_ht=instagram.fiev1-1.fna.fbcdn.net&_nc_ohc=p6pOrtMcKlUAX9euqFy&oh=c3b4121d1fd7f30e1840b2bcd99e7718&oe=5F67768F" alt="" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{ Auth::user()->username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="#">Add new post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>998m</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">Daniel</div>
-            <div class="">the worms will come for you</div>
-            <div><a href="https://www.youtube.com/watch?v=GYgAvngxzMw">youtu.be/GYgAvngxzMw</a></div>
+            <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
+            <div class="">{{$user->profile->description}}</div>
+            <div><a href="{{$user->profile->url}}">{{$user->profile->url}}</a></div>
         </div>
     </div>
 
