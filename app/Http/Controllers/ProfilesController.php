4a19d33f3d3a81577dc569b26c56c15e6987eec9
Daniel Zabavchuk <d.p.zabavchuk@gmail.com>
2020-09-03 19:18:09 +0300

    Edit profile

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -5,16 +5,55 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Profile;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
-    public function index($user_id)
+    public function index(User $user)
     {
 
-        $template = [
-            'user' => User::findOrFail($user_id)
-        ];
+//        $template = [
+//            'user' => User::findOrFail($user_id)
+//        ];
+//
+//        return view('profiles/index', $template);
+        return view('profiles/index', compact('user'));
+    }
 
-        return view('profiles/index', $template);
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user->profile);
+
+        return view('profiles/edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $this->authorize('update', $user->profile);
+
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+//            'image' => ['required', 'image']
+            'image' => ''
+        ]);
+
+        $imagePath = '';
+
+        if (request('image')){
+
+            $imagePath = request('image')->store('profile', 'public');
+
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
+            $image->save();
+        }
+
+        auth()->user()->profile()->update(array_merge(
+            $data,
+            ['image' => $imagePath]
+            ));
+
+        return redirect("/profile/{$user->id}");
     }
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,14 +4,18 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://instagram.fiev1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/34565799_438171569943658_196224265341632512_n.jpg?_nc_ht=instagram.fiev1-1.fna.fbcdn.net&_nc_ohc=p6pOrtMcKlUAX9euqFy&oh=c3b4121d1fd7f30e1840b2bcd99e7718&oe=5F67768F" alt="" class="rounded-circle">
+            <img src="/storage/{{$user->profile->image}}" alt="" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="/p/create">Add new post</a>
+                @can('update', $user->profile)
+                    <a href="/p/create">Add new post</a>
+                @endcan
             </div>
-            <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
+            @can('update', $user->profile)
+                <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
+            @endcan
             <div class="d-flex">
                 <div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts</div>
                 <div class="pr-5"><strong>998m</strong> followers</div>
