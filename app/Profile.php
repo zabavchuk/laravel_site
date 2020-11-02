113bd99d7fe5f038c4a8cf229223417115ef44f4
Daniel Zabavchuk <d.p.zabavchuk@gmail.com>
2020-11-02 16:56:06 +0200

    adding default photo

--- app/Profile.php
+++ app/Profile.php
@@ -9,6 +9,12 @@ class Profile extends Model
 
     protected $guarded = [];
 
+    public function profileImage(){
+
+        $imagePath = $this->image ? $this->image : 'profile/480px-No_image_available.svg.png';
+        return $imagePath;
+    }
+
     public function user(){
         return $this->belongsTo(User::class);
     }
